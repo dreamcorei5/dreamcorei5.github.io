9343674a92a28be8086699b6abfd742051027958
Dreamcorei5 <dreamcorei5@hotmail.com>
2020-01-09 14:32:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
-// self.addEventListener('install', () => self.skipWaiting());
-// self.addEventListener('activate', () => self.clients.claim());
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(
-        [
-          // '/css/bootstrap.css',
-          // '/css/main.css',
-          // '/js/bootstrap.min.js',
-          // '/js/jquery.min.js',
-          '/index.html'
-          // Add anything else you need to be cached during the SW install
-        ]
-      );
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/index.html',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
