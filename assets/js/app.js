8f811fc80dcd9bd0abc00e65aed10c17e9d3d4b6
Khunathorn Thongnopphakhun <khunathorntongnopphakhun@gmail.com>
2021-01-27 04:38:34 +0700

    Summary

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,37 +2,43 @@ let modal = document.getElementById("myModal");
 let modalImg = document.getElementById("img01");
 
 let corpdevice = document.getElementById("corpdevice");
-corpdevice.onclick = function(){
+corpdevice.onclick = () => {
   modal.style.display = "block";
   modalImg.src = 'assets/img/corpdevice.png';
 }
 
 let coseller = document.getElementById("coseller");
-coseller.onclick = function(){
+coseller.onclick = () => {
   modal.style.display = "block";
   modalImg.src = 'assets/img/coseller.png';
 }
 
 let kasetworld = document.getElementById("kasetworld");
-kasetworld.onclick = function(){
+kasetworld.onclick = () => {
   modal.style.display = "block";
   modalImg.src = 'assets/img/kasetworld.png';
 }
 
 let manage_language = document.getElementById("manage_language");
-manage_language.onclick = function(){
+manage_language.onclick = () => {
   modal.style.display = "block";
   modalImg.src = 'assets/img/manage_language.png';
 }
 
 let bpskill = document.getElementById("bpskill");
-bpskill.onclick = function(){
+bpskill.onclick = () => {
   modal.style.display = "block";
   modalImg.src = 'assets/img/bpskill.png';
 }
 
+let manage_web = document.getElementById("manage_web");
+manage_web.onclick = () => {
+  modal.style.display = "block";
+  modalImg.src = 'assets/img/manage_web.png';
+}
+
 let plagiarism = document.getElementById("plagiarism");
-plagiarism.onclick = function(){
+plagiarism.onclick = () => {
   modal.style.display = "block";
   modalImg.src = 'assets/img/plagiarism.png';
 }
