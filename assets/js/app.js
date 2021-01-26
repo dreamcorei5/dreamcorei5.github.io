3a213147b9a271931f8f96ec05b27d3efe61691b
Khunathorn Thongnopphakhun <khunathorntongnopphakhun@gmail.com>
2021-01-27 04:27:36 +0700

    Summary

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,34 @@ corpdevice.onclick = function(){
   modalImg.src = 'assets/img/corpdevice.png';
 }
 
+let coseller = document.getElementById("coseller");
+coseller.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = 'assets/img/coseller.png';
+}
+
+let kasetworld = document.getElementById("kasetworld");
+kasetworld.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = 'assets/img/kasetworld.png';
+}
+
+let manage_language = document.getElementById("manage_language");
+manage_language.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = 'assets/img/manage_language.png';
+}
+
 let bpskill = document.getElementById("bpskill");
 bpskill.onclick = function(){
   modal.style.display = "block";
   modalImg.src = 'assets/img/bpskill.png';
+}
+
+let plagiarism = document.getElementById("plagiarism");
+plagiarism.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = 'assets/img/plagiarism.png';
 }
 
 let span = document.getElementsByClassName("close")[0];
